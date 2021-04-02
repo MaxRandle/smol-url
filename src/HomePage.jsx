303262fff0ee226ea5ab7ee90be23ee08974f877
maxrandle <maxrandle95@gmail.com>
2021-04-02 23:23:51 +1300

    change URL validation regex on front and back end

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -71,10 +71,10 @@ const HomePage = () => {
   const longUrlTests = [
     {
       test: (value) =>
-        /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/.test(
+        /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(
           value
         ),
-      onFailFeedback: "that's not a valid url D:<",
+      onFailFeedback: "that's not a url :(",
     },
   ];
 
